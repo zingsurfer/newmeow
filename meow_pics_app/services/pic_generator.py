cbc6288a149d5a9dfb485a22ecba2c049faa46bf
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-06 16:18:19 -0700

    Refactor by renaming method to be specific & removing unnec code

--- meow_pics_app/services/pic_generator.py
+++ meow_pics_app/services/pic_generator.py
@@ -1,14 +1,8 @@
-# import os
-# import requests
 from .cat_api_service import CatAPIService
+
 class PicGenerator:
 
     def random():
-        # api_url = 'https://api.thecatapi.com/v1/images/search'
-        # headers = {'x-api-key': f'{os.environ.get("CAT_KEY")}'}
-        # response = requests.get(api_url, headers=headers)
-        # data = response.json()
-        # pic_url = data[0]["url"]
-        pic_url = CatAPIService.random_cat_url()
-        
+        pic_url = CatAPIService.random_cat_pic_url()
+
         return pic_url
