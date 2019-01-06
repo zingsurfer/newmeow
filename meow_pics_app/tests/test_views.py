ab93f8b118333c025ffd20b9d24ef4db1d6affa0
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-05 21:33:06 -0700

    Test home page view

---
+++ meow_pics_app/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+from .. import views
+from meow_pics_app.views import HomePageView
+
+client = Client()
+
+class HomePageTest(TestCase):
+    def test_root_url_renders_home_page(self):
+        home_page = client.get(reverse('home'))
+        
+        self.assertTrue(home_page.status_code, 200)
+        self.assertTemplateUsed(home_page, 'home.html')
