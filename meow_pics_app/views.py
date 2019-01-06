5fd9cd55df8e1eb6cfd778c172850400e8a2299b
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-06 14:52:59 -0700

    Connect to the Cat API

--- meow_pics_app/views.py
+++ meow_pics_app/views.py
@@ -1,7 +1,14 @@
+import os
 import requests
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     def get(self, request):
-        return render(request, 'home.html')
+        api_url = 'https://api.thecatapi.com/v1/images/search'
+        headers = {'x-api-key': f'{os.environ.get("CAT_KEY")}'}
+        response = requests.get(api_url, headers=headers)
+        data = response.json()
+        pic_url = data[0]["url"]
+
+        return render(request, 'home.html',{'pic':pic_url})
