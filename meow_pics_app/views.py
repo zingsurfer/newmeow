7f685a4e3c332a87869f01f328cf55ab38f742ed
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-06 16:40:46 -0700

    Remove unused code

--- meow_pics_app/views.py
+++ meow_pics_app/views.py
@@ -1,4 +1,3 @@
-import os
 import requests
 from django.shortcuts import render
 from django.views.generic import TemplateView
@@ -6,10 +5,6 @@ from .services.pic_generator import PicGenerator
 
 class HomePageView(TemplateView):
     def get(self, request):
-        # api_url = 'https://api.thecatapi.com/v1/images/search'
-        # headers = {'x-api-key': f'{os.environ.get("CAT_KEY")}'}
-        # response = requests.get(api_url, headers=headers)
-        # data = response.json()
-        # pic_url = data[0]["url"]
         random_cat_pic = PicGenerator.random
+
         return render(request, 'home.html',{'random_cat_pic':random_cat_pic})
