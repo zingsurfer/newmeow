d1b664ed83f2250f32aee3e6af889400db731402
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-05 17:41:22 -0700

    Update home page test with class to test for appname in title

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,18 @@
+import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+class HomePageTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_view_home_page(self):
+        self.browser.get('http://localhost:8080')
+        self.assertIn('NewMeow', self.browser.title)
+        # fail regardless until test is finished being written
+        self.fail('Is this test finished being written?')
 
-browser.get('http://localhost:8080')
-
-assert 'Django' in browser.title
+# if this file is executed from CLI, launch unittest to run tests
+if __name__ == '__main__':
+    unittest.main()
