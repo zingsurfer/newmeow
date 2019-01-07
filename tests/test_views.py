410b97bfbc067f8ca2ca4bd9020fd0b8f97a6239
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-06 20:59:46 -0700

    Test view for favorite meow pics

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from meow_pics_app.views import HomePageView
+from meow_pics_app.views import HomePageView, FaveMeowPicsPageView
 
 client = Client()
 
@@ -11,3 +11,10 @@ class HomePageTest(TestCase):
 
         self.assertTrue(home_page.status_code, 200)
         self.assertTemplateUsed(home_page, 'home.html')
+
+class FaveMeowPicsPageTest(TestCase):
+    def test_favorite_meow_pics_url_renders_favorite_meow_pics_page(self):
+        favorites_page = client.get(reverse('fave_meow_pics'))
+
+        self.assertTrue(favorites_page.status_code, 200)
+        self.assertTemplateUsed(favorites_page, 'fave_meow_pics/index.html')
