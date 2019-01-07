2d377b203722b5f1727f18f5c236723ffac23ac2
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-06 20:15:23 -0700

    Add created at & updated at attributes

--- meow_pics_app/models.py
+++ meow_pics_app/models.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class FavoriteCatPic(models.Model):
     url = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
