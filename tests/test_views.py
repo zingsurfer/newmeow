9c15f7be808c241abaf20feef5f2e8079937853c
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-07 16:56:18 -0700

    Update tests for views

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,7 +1,9 @@
 from django.test import Client, TestCase
+from django.test.client import RequestFactory
 from django.urls import reverse
 
-from meow_pics_app.views import HomePageView, FaveMeowPicsPageView
+from meow_pics_app.models import FavoriteCatPic
+from meow_pics_app.views import HomePageView, FaveMeowPicsIndex
 
 client = Client()
 
@@ -15,6 +17,8 @@ class HomePageTest(TestCase):
 class FaveMeowPicsPageTest(TestCase):
     def test_favorite_meow_pics_url_renders_favorite_meow_pics_page(self):
         favorites_page = client.get(reverse('fave_meow_pics'))
-
+        cat_pic = FavoriteCatPic.objects.create(url="https://cdn2.thecatapi.com/images/123.jpg")
+        print(FavoriteCatPic.objects.all())
         self.assertTrue(favorites_page.status_code, 200)
         self.assertTemplateUsed(favorites_page, 'fave_meow_pics/index.html')
+        self.assertContains(favorites_page, "Fave Meow Pics")
