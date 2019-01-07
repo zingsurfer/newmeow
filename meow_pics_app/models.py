a315bead28920b172b6ec431a2dd6b76ff5bafc9
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-07 12:56:24 -0700

    Add __str__ method to return the string representation of urls

--- meow_pics_app/models.py
+++ meow_pics_app/models.py
@@ -4,3 +4,6 @@ class FavoriteCatPic(models.Model):
     url = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.url)
