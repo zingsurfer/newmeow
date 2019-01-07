c6430b6071ca59cdfc6557d518b5766576daf497
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-06 21:02:17 -0700

    Add view for favorite meow pics

--- meow_pics_app/views.py
+++ meow_pics_app/views.py
@@ -8,3 +8,7 @@ class HomePageView(TemplateView):
         random_cat_pic = PicGenerator.random
 
         return render(request, 'home.html',{'random_cat_pic':random_cat_pic})
+
+class FaveMeowPicsPageView(TemplateView):
+    def get(self, request):
+        return render(request, 'fave_meow_pics/index.html')
