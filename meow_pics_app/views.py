f146e46680e68614ae786a0444daca25204ad923
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-07 00:24:36 -0700

    Add array to test favorites template with

--- meow_pics_app/views.py
+++ meow_pics_app/views.py
@@ -1,6 +1,7 @@
 import requests
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from meow_pics_app.models import FavoriteCatPic
+from django.views.generic import TemplateView, ListView
 from .services.pic_generator import PicGenerator
 
 class HomePageView(TemplateView):
@@ -9,6 +10,11 @@ class HomePageView(TemplateView):
 
         return render(request, 'home.html',{'random_cat_pic':random_cat_pic})
 
-class FaveMeowPicsPageView(TemplateView):
+class FaveMeowPicsIndex(ListView):
     def get(self, request):
-        return render(request, 'fave_meow_pics/index.html')
+        model = FavoriteCatPic
+        # fave_pics = FavoriteCatPic.objects
+        # return render(request, 'fave_meow_pics/index.html')
+        template_name = 'fave_meow_pics/index.html'
+        fave_pics = ["https://cdn2.thecatapi.com/images/265.jpg", "https://cdn2.thecatapi.com/images/123.jpg"]
+        return render(request, template_name, {'fave_meow_pics':fave_pics})
