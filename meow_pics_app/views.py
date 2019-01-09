17163ca9d4d16a80186a851f6bdeaebeee835d2a
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-09 00:25:04 -0700

    Progress on favorite feature: implement user flow

--- meow_pics_app/views.py
+++ meow_pics_app/views.py
@@ -1,7 +1,13 @@
 import requests
 from django.shortcuts import render
-from meow_pics_app.models import FavoriteCatPic
 from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView
+
+from django.urls import reverse_lazy
+from django import forms
+from django.http import HttpResponseRedirect
+
+from meow_pics_app.models import FavoriteCatPic
 from .services.pic_generator import PicGenerator
 
 class HomePageView(TemplateView):
@@ -18,3 +24,18 @@ class FaveMeowPicsIndex(ListView):
         fave_meow_pics = FavoriteCatPic.objects.all()
 
         return fave_meow_pics
+
+def NewFaveMeowPics(request):
+    if request.method == 'POST':
+        form = FavoriteForm(request.POST)
+        form.save
+        return HttpResponseRedirect('/fave_meow_pics')
+
+class FavoriteForm(forms.ModelForm):
+# class FavoriteForm(forms.Form):
+    class Meta:
+        model = FavoriteCatPic
+        fields = ['url']
+        # widgets = {'url': forms.HiddenInput()}
+        # url = forms.HiddenInput()
+        # hidden_field = forms.CharField()
