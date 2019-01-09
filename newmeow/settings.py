fe0d93e71f693c704c54e9df3ca5276b376d8268
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-09 02:11:13 -0700

    Configure django-heroku in settings

--- newmeow/settings.py
+++ newmeow/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -130,3 +130,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+django_heroku.settings(locals())
